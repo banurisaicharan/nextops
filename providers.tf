3d8ec78c0d0f5a6421ce5d3af1f4a8852d9620e6
banurisaicharan <banurisaicharan@gmail.com>
2025-02-01 11:52:58 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,21 +1,13 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.17.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.84.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "8620c198-bbac-4767-9eef-c12a1f8ceac8"
-}
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "myresource"
-    storage_account_name = "terraformstorage689"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+provider "aws" {
+access_key = "AKIA3SYIDKJMC6QKV5TF"
+secret_key = "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"
 }
